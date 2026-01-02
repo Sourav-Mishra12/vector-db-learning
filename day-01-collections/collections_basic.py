e001feaa065093b537dc9b3e3feadb8eaba481f2
sourav mishra <cag.souravmishra@gmail.com>
2026-01-02 13:04:15 +0530

    Quantization

--- day-01-collections/collections_basic.py
+++ day-01-collections/collections_basic.py
@@ -120,3 +120,83 @@ client.update_collection(
    optimizers_config=models.OptimizersConfigDiff(indexing_threshold=10000),
 )
 '''
+
+'''
+UPDATING VECTOR PARAMETERS
+
+client.update_collection(
+    collection_name="{collection_name}",
+    vectors_config={
+        "my_vector": models.VectorParamsDiff(
+            hnsw_config=models.HnswConfigDiff(
+                m=32,
+                ef_construct=123,
+            ),
+            quantization_config=models.ProductQuantization(
+                product=models.ProductQuantizationConfig(
+                    compression=models.CompressionRatio.X32,
+                    always_ram=True,
+                ),
+            ),
+            on_disk=True,
+        ),
+    },
+    hnsw_config=models.HnswConfigDiff(
+        ef_construct=123,
+    ),
+    quantization_config=models.ScalarQuantization(
+        scalar=models.ScalarQuantizationConfig(
+            type=models.ScalarType.INT8,
+            quantile=0.8,
+            always_ram=False,
+        ),
+    ),
+)
+
+While updating vector name should be mentioned .
+
+models.HnswConfigDiff(
+    m=32,
+    ef_construct=123
+)
+
+INTUITION :
+    
+     m -> graph connectivity (higher means , better recall and large memory)
+     ef_construct -> index build quality (slow build , better index)
+
+Quantization :- 
+                it is used to compress the vectors (precision less , memory less)
+
+In this we can see 2 types of quantization:
+
+PRODUCT QUANTIZATION-
+
+models.ProductQuantization(
+      product = models.ProductQuantizationConfig(
+           compression= models.CompressionRatio.X32,
+           always_ram= True
+      )
+)
+meaning :- breaking down the vectors in small parts . Memory usage lowers as well as the accuracy
+
+use case :- Use when scaling hugely , cost sensitivity , recall drop is acceptable
+
+SCALAR QUANTIZATION -
+
+models.ScalarQuantization(
+
+      scalar = models.ScalarQuantizationConfig(
+           type= models.ScalarType.INT8,
+           quantile = 0.8,
+           always_ram = False,
+      )
+)
+
+meaning :- conversion from float -> UINT8 , makes it simpler and faster and it is moderate compression
+
+use case :- want balanced performance and common choice in RAG systems
+
+VECTOR LEVEL ALWAYS OVERRIDES COLLECTION LEVEL
+
+'''
